76bb22d3db294a2e65ae40d861911e85bf458b1a
fridaluz22 <fridaluzf14@gmail.com>
2024-07-04 22:58:41 -0500

    Fisrt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularProyect-app';
+  http = inject(HttpClient);
+
+
+  changeTitle(){
+    this.title = 'Frida Luz';
+
+
+  }
+
+  ngOnInit(){
+    this.http.get('https://api.escuelajs.co/api/v1/products')
+    .subscribe((data: any)=>{
+
+    })
+  }
 }
