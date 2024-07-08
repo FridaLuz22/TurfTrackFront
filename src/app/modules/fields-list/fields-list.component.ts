cb57ccdb14bdec1e432e0b479a0aeb666e882f6c
fridaluz22 <fridaluzf14@gmail.com>
2024-07-07 23:39:06 -0500

    armar los modulos y componentes

---
+++ src/app/modules/fields-list/fields-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fields-list',
+  templateUrl: './fields-list.component.html',
+  styleUrls: ['./fields-list.component.css']
+})
+export class FieldsListComponent {
+
+}
